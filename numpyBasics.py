6dedf7c5977d3781eb19a1f7f1c32a326848aa78
XabiChettiar <chettiar.bosco07@gmail.com>
2026-08-25 12:15:21 +0530

    Learned numpy basics

---
+++ numpyBasics.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr=np.array([1,2,3,4])
+
+a=np.zeros(5)
+b=np.ones(5)
+c=np.arange(1,6)
+
+matrix=np.array([[1,2],[3,4]])
+tensor=np.array([[1,2],[3,4],[5,6],[7,8]])
+
+print(type(arr))
+print(matrix)
+print(tensor)
+
+
